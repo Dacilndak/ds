692f6da5c994a4354581f066316f34a3625dc5ba
mahaney <mahatthew.haney@colorado.edu>
2015-09-18 15:38:45 -0600

    Added queues

---
+++ include/queue.hpp
@@ -0,0 +1,9 @@
+#ifndef _MPH_WRAPPER_QUEUE_H_
+#define _MPH_WRAPPER_QUEUE_H_
+
+#include <queue/queue.hpp>
+#include <queue/deque.hpp>
+
+template <typename T> using DoubleQueue = Deque<T>;
+
+#endif
---
+++ include/queue/queue.hpp
@@ -0,0 +1,47 @@
+#ifndef _MPH_QUEUE_H_
+#define _MPH_QUEUE_H_
+
+#include <list.hpp>
+
+template <typename T>
+class Queue {
+private:
+  List<T> contents;
+  const int size;
+public:
+  Queue() : size(0) { };
+  Queue(int s) : size(s) { };
+  Queue(List<T> data) : size(data.size()) { };
+  Queue(Queue<T> q) : size(q.size()) { };
+  Queue(List<T> data, int s) : size(s > data.size() ? s : data.size()) { };
+  Queue(Queue<T> data, int s) : size(s > data.length() ? s : data.size()) { };
+  ~Queue() { };
+  const int size() const { return size; }
+  int length() const { return contents.size() };
+  bool is_empty() const { return this->length() == 0; }
+  bool is_full() const { return this->length() == this->size(); }
+  T peek() const { return contents.get(contents.size()); }
+  T pop();
+  bool push(T data);
+};
+
+template <typename T>
+T Queue::pop() {
+  if (is_empty()) {
+    return (T)(0);
+  }
+  T data = contents.at(0);
+  contents.remove(0);
+  return data;
+}
+
+template <typename T>
+bool Queue::push(T data) {
+  if (is_full()) {
+    return false;
+  }
+  contents.append(data);
+  return true;
+}
+
+#endif
